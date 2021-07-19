a84c9b3ca953ceea64599c866fc097db5e86f986
sxhmkoul <81112212+sxhmkoul@users.noreply.github.com>
2021-07-19 09:44:10 +0530

    Assignment 1
    Angularjs

---
+++ app.js
@@ -0,0 +1,50 @@
+(function(){
+    'use strict';
+
+    angular.module("lunchApp",[])
+    .controller("lunchAppController", lunchAppController);
+
+    lunchAppController.$inject = ['$scope'];
+    
+    function lunchAppController($scope){
+        $scope.lunchMenu = "";
+        $scope.result = "";
+        $scope.val = "";
+        $scope.increment = 0;
+        $scope.counter = 0;
+
+
+
+        $scope.clickBtn = function(){
+            $scope.lunchChecker();
+            $scope.validatorFun();
+            
+        }
+
+
+
+        $scope.validateKey = function(){
+           var len = $scope.lunchMenu.length;
+           $scope.counter = len;
+           $scope.validatorFun();
+        }
+
+        $scope.validatorFun = function(){
+            if($scope.counter === 0){
+                $scope.val = "Please enter a valid input!";
+                $scope.result = "";
+            } else $scope.val = "";
+        }
+
+        $scope.lunchChecker = function(){
+            var menu = ($scope.lunchMenu).split(",");
+            if(menu.length>3){
+                $scope.result = "You ate too much!";
+            } else $scope.result = "Enjoy!";
+
+        }
+
+
+
+    }
+})();
